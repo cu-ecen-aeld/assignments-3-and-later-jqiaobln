8d363ef3328f46a65db0058099f8831610a3cd58
jqiaobln <jqiao@blacklinesafety.com>
2023-11-18 14:01:56 -0700

    Added aesdsocket-start-stop script

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Path to the aesdsocket executable
+AESDSOCKET="/path/to/aesdsocket"
+
+# Function to start the aesdsocket daemon
+start() {
+    start-stop-daemon --start --background --exec "$AESDSOCKET" -- -d
+}
+
+# Function to stop the aesdsocket daemon
+stop() {
+    start-stop-daemon --stop --signal SIGTERM --exec "$AESDSOCKET"
+}
+
+# Check the command-line argument
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
